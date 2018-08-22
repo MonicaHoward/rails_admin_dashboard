86bfa29ce5b8110278c240f79875cee88438c3ee
Monica Howard <jay.m.howard@gmail.com>
2018-08-22 16:02:08 -0500

    Established association between User and Doc tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822132507) do
+ActiveRecord::Schema.define(version: 20180822204841) do
 
   create_table "docs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
