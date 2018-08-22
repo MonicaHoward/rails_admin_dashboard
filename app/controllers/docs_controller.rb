aa5729f950aa016f515dfb1e956b4c15245540ba
Monica Howard <jay.m.howard@gmail.com>
2018-08-22 08:07:31 -0500

    updated actions in doc controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,5 @@
 class DocsController < ApplicationController
-  before_action :find_doc, only: [:show, :edit, :update, :destory]
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
     @docs = Doc.all.order("created_at DESC")
@@ -27,7 +27,7 @@ class DocsController < ApplicationController
   end
 
   def update
-    if @doc.update(doc_param)
+    if @doc.update(doc_params)
       redirect_to @doc
     else
       render "edit"
