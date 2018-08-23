e36ebea4ad95d594f00218bbb51d51e953740fe0
Monica Howard <jay.m.howard@gmail.com>
2018-08-22 21:33:10 -0500

    Added additional stylesheets and javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require main
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
